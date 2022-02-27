d2f012a65e426cc8330935bcd7ec2201afaf174f
AanchalCh <aanchal.chaurasia99@gmail.com>
2022-02-27 11:22:03 +0530

    Refactoring code, managing escape key events

--- script.js
+++ script.js
@@ -2,35 +2,29 @@
 
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
-const buttonCloseModal = document.querySelector('.close-modal') 
-
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
-const openModal = function (){
-    console.log ("Button clicked")
+const closeBtn = document.querySelector('.close-modal')
+
+const openModal = function() {
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden')
-}
-
-const closeModal = function (){
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
 }
 
 for(let i=0; i<btnsOpenModal.length; i++){
     btnsOpenModal[i].addEventListener('click', openModal)
 }
 
-buttonCloseModal.addEventListener('click', closeModal)
+const closeModal = function() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+closeBtn.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
 
 document.addEventListener('keydown', function(e){
-    console.log(e.key)
-
-    //if the pressed key is escape
-    if(e.key === 'Escape'){
-        //if the modal does not contain the hidden class then close the modal
-        if(!modal.classList.contains('hidden'))
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal()
     }
 })
