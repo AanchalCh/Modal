6858faabfc0d7fb5f2d8c575c792a64ed05badca
AanchalCh <aanchal.chaurasia99@gmail.com>
2022-02-27 10:44:04 +0530

    Adding event listeners for key press

--- script.js
+++ script.js
@@ -23,3 +23,14 @@ for(let i=0; i<btnsOpenModal.length; i++){
 
 buttonCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e){
+    console.log(e.key)
+
+    //if the pressed key is escape
+    if(e.key === 'Escape'){
+        //if the modal does not contain the hidden class then close the modal
+        if(!modal.classList.contains('hidden'))
+        closeModal()
+    }
+})
